0bc16074068b19944048889f97d3d5d73f4a68a1
Zahar Andrienko <zaharandrienko88@gmail.com>
2020-08-16 22:09:52 +0300

    Updates

---
+++ precache-manifest.274324136eaeacade8aab9beda8ed1b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d83963f69f19f87888daef12f514c94",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "84f82a60d75492c1c18c",
+    "url": "/my-app/static/css/main.aabf6c3f.chunk.css"
+  },
+  {
+    "revision": "38e26b715cf465ed428b",
+    "url": "/my-app/static/js/2.e7c12280.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/my-app/static/js/2.e7c12280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f82a60d75492c1c18c",
+    "url": "/my-app/static/js/main.4feb3a1c.chunk.js"
+  },
+  {
+    "revision": "b3318afa56886e773112",
+    "url": "/my-app/static/js/runtime-main.760a801a.js"
+  },
+  {
+    "revision": "b1f418009d44b897186043faf5c6ebc1",
+    "url": "/my-app/static/media/1.b1f41800.jpg"
+  },
+  {
+    "revision": "87438eac41089713988d3df234f7dd5a",
+    "url": "/my-app/static/media/6.87438eac.jpg"
+  },
+  {
+    "revision": "54628b207611a6e376f3eb738a1be0cc",
+    "url": "/my-app/static/media/YS.54628b20.jpg"
+  },
+  {
+    "revision": "9d130a0d2e179f9af522563154b6fc3e",
+    "url": "/my-app/static/media/bogdan2.9d130a0d.jpg"
+  },
+  {
+    "revision": "46275b40c55a5266749c119287cbb84d",
+    "url": "/my-app/static/media/homeBack.46275b40.png"
+  }
+]);
